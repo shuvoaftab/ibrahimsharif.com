ef8644844852f1f3db5488ded7cd2be909253e19
Ibrahim Sharif <shuvoaftab@gmail.com>
2023-10-01 01:21:50 +0600

    Footer Year, about, contact v1.0

---
+++ assets/js/custom.js
@@ -0,0 +1,6 @@
+
+const copyrightText = `
+<p class="text-center">&copy; ${new Date().getFullYear()} |  Ibrahim Sharif | All rights reserved.</p>
+`;
+
+document.getElementById('copyright').innerHTML = copyrightText;
