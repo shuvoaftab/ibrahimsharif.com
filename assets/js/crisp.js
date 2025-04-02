da7f6fa4059fc05b22b90e2624506e0038e75bb3
Ibrahim Sharif <75041519+aftabshuvo@users.noreply.github.com>
2025-04-03 01:38:02 +0600

    v0.0.4

---
+++ assets/js/crisp.js
@@ -0,0 +1,1 @@
+window.$crisp = []; window.CRISP_WEBSITE_ID = "8d20914b-0995-4bdd-aa36-a2274ec8cca6"; (function () { d = document; s = d.createElement("script"); s.src = "https://client.crisp.chat/l.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+const veryLongInterval = 10000;
 const longInterval = 5000;
 const shortInterval = 2000;
 const veryShortInterval = 1000;
@@ -8,14 +9,32 @@ let baseStyle = "assets/css/style.css";
 let rubikFont = "https://fonts.googleapis.com/css2?family=Rubik:wght@400;600&display=swap";
 let robotoFont = "https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap";
 
+let crispChat = "assets/js/crisp.js"; // Crisp Chat Script
 
+// Load Stylesheets in the order of importance
 // window.load = delayedStyleSheetLoading(compiledStyle, veryShortInterval );
 // window.load = delayedStyleSheetLoading(baseStyle, shortInterval );
-window.load = delayedStyleSheetLoading(extraStyle, longInterval );
-window.load = delayedStyleSheetLoading(rubikFont, longInterval );
-window.load = delayedStyleSheetLoading(robotoFont, longInterval );
+// window.load = delayedStyleSheetLoading(extraStyle, veryLongInterval );
+// window.load = delayedStyleSheetLoading(rubikFont, veryLongInterval );
+// window.load = delayedStyleSheetLoading(robotoFont, veryLongInterval );
 
 
+// Load Stylesheets after the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    delayedStyleSheetLoading(extraStyle, veryLongInterval );
+    delayedStyleSheetLoading(rubikFont, veryLongInterval );
+    delayedStyleSheetLoading(robotoFont, veryLongInterval );
+    delayedScriptLoading(crispChat, veryLongInterval );
+});
+
+
+
+/**
+ * Dynamically loads a stylesheet into the document after a specified delay.
+ *
+ * @param {string} stylesheetPath - The path to the stylesheet to be loaded.
+ * @param {number} intervalTime - The delay in milliseconds before the stylesheet is loaded.
+ */
 function delayedStyleSheetLoading(stylesheetPath, intervalTime) {
     // const intervalTime = 10000;
     // const stylesheetPath = "assets/css/main.css";
@@ -26,3 +45,29 @@ function delayedStyleSheetLoading(stylesheetPath, intervalTime) {
         document.head.appendChild(stylesheet);
     }, intervalTime);
 }
+
+/**
+ * Use requestIdleCallback (More Performance-Friendly)
+ * This method loads the script when the browser is idle.
+ * 
+ * @param {string} src - The source URL of the script to load.
+ * @param {number} delay - The delay in milliseconds before loading the script.
+ */
+function loadScript(src) {
+    var script = document.createElement("script");
+    script.src = src;
+    script.async = true;
+    document.body.appendChild(script);
+}
+
+//  Load the script after the DOM is fully loaded => Crisp Chat
+// if ('requestIdleCallback' in window) {
+//     requestIdleCallback(() => loadScript(crispChat));
+// } else {
+//     setTimeout(() => loadScript(crispChat), veryLongInterval); // Fallback
+// }
+
+function delayedScriptLoading(src, delay) {
+    setTimeout(() => loadScript(src), delay); // Fallback
+}
+
